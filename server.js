fcd6c2f5d455e8bbfdf7a87d27d112f3ff9e1901
Rohan Sahani <rohan.sahani.m97@gmail.com>
2021-04-09 04:41:54 +0530

    rr

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ app.post('/imageurl',(req,res)=>{image.handleApi(req,res)})
 
 
 app.listen(process.env.PORT || 3000,()=>{
-    console.log(`app is running ${process.env.PORT}`);
+    console.log(`app is running on port ${process.env.PORT}`);
 });
